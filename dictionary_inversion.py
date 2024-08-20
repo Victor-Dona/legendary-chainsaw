60e0d9e1655e592d27fd9f8f86ade8a42480d8ce
Victor Dona <158128371+Victor-Dona@users.noreply.github.com>
2024-08-19 22:02:56 -0500

    Create dictionary_inversion.py
    Script that inverts key:value pairs in a dictionary.

---
+++ dictionary_inversion.py
@@ -0,0 +1,9 @@
+country_capitals = {
+  "Germany": "Berlin", 
+  "Canada": "Ottawa", 
+  "England": "London"
+  }
+
+capitals_country = {value:key for key, value in country_capitals.items()}
+print(capitals_country)
+
